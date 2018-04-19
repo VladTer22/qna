1ee73f0748f55aedf88920f807f37ae7d6fff6fe
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-04-19 12:11:18 +0100

    Installed devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe User do
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+RSpec.describe User, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
