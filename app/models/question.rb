366f5da9ca279c2c9aa7eca6702c797c350790a0
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-04-21 12:11:18 +0100

    Added user to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,12 @@
 class Question < ApplicationRecord
   validates :title, :body, presence: true
-  has_many :answers
+  has_many :answers, dependent: :delete_all
 
   def short_body
     "#{body.truncate(57)}..."
+  end
+
+  def user
+    User.find(user_id).email
   end
 end
