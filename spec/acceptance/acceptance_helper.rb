5a34f1d742d208bceab2b8046b2a2bc98fd5d807
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-04-25 12:11:18 +0100

    Created helper for acceptance tests and configured DBCleaner

---
+++ spec/acceptance/acceptance_helper.rb
@@ -0,0 +1,25 @@
+require_relative 'rails_helper'
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suit) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
