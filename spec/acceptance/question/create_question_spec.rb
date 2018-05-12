83d2774f12b446217a96243e4bf3dc4c360b619f
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-12 12:11:18 +0100

    Created question channel test

--- spec/acceptance/question/create_question_spec.rb
+++ spec/acceptance/question/create_question_spec.rb
@@ -27,4 +27,23 @@ feature 'Create question', '
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 
+  scenario 'Multiple sessions', js: true do
+    Capybara.using_session('user') do
+      sign_in(user)
+      visit questions_path
+    end
+    Capybara.using_session('guest') do
+      visit questions_path
+    end
+
+    Capybara.using_session('user') do
+      click_on 'Ask a question'
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text'
+      click_on 'Ask!'
+    end
+    Capybara.using_session('guest') do
+      expect(page).to have_content 'Test question'
+    end
+  end
 end
