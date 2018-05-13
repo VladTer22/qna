3b7dda63092dd8acf046ce9bd2cfe6dd32d5349c
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-13 12:11:18 +0100

    Created questions channel

---
+++ app/javascript/channels/question_channel.js
@@ -0,0 +1,25 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("QuestionChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+    this.perform('follow')
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    let table = $('.questions_list')
+    let question = data
+    let string = `<tr>
+    <td>` + question.title + `</td>
+    <td>` + question.short_body + `</td>
+    <td>` + question.user + `</td>
+    <td><a href="`+ question.question_path +`">Show</a></td>`
+    if(question.current_user != 0)
+      string += `<td><a href="`+ question.question_path +`" data-method="delete">Destroy</a></td>`
+    table.append(string)
+  }
+});
