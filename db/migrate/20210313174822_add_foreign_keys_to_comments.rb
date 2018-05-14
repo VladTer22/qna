59a3ec280b5c42c1f1e051a6a7d8d9de3a67b1bf
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-14 12:11:18 +0100

    Fixed index adding

--- db/migrate/20210313174822_add_foreign_keys_to_comments.rb
+++ db/migrate/20210313174822_add_foreign_keys_to_comments.rb
@@ -3,6 +3,7 @@ class AddForeignKeysToComments < ActiveRecord::Migration[6.1]
     add_column :comments, :commentable_id, :integer
     add_column :comments, :commentable_type, :string
     add_index :comments, %i[commentable_id commentable_type]
-    add_column :comments, :user_id, :integer, index: true
+    add_column :comments, :user_id, :integer
+    add_index :comments, :user_id
   end
 end
