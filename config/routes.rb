0e4c4f60dd6acdc75d1682f8f02f046a57cac513
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-16 12:11:18 +0100

    Added routes for creating comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  get 'comments/create'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'questions#index'
 
   resources :questions do
+    post :comment_question, to: 'comments#comment_question'
     member do
       delete :remove_attachment
     end
     resources :answers do
+      post :comment_answer, to: 'comments#comment_answer'
       member do
         post :set_best
         patch :upvote
@@ -16,6 +19,9 @@ Rails.application.routes.draw do
       end
     end
   end
+
+
+  post '/comments/', to: 'comments#create_question_comment'
 
   mount ActionCable.server => '/cable'
 end
