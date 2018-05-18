244743fbf06f22b0d3a21be1b6786a0445ade130
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-18 12:11:18 +0100

    Created module macros for omniauth tests

---
+++ spec/support/omniauth_macros.rb
@@ -0,0 +1,19 @@
+module OmniauthMacros
+  def mock_auto_hash
+    # The mock_auth configuration allows you to set per-provider (or default)
+    # authentication hashes to return during integration testing.
+    OmniAuth.config.mock_auth[:github] = {
+      provider: 'github',
+      uid: '123545',
+      info: {
+        name: 'mockuser',
+        image: 'mock_user_thumbnail_url',
+        email: 'test@test.com'
+      },
+      credentials: {
+        token: 'mock_token',
+        secret: 'mock_secret'
+      }
+    }
+  end
+end
