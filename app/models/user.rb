e1c617ae535ceda89e4f588db5da273ee097c725
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-19 12:11:18 +0100

    Added github as one of the options

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,29 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :omniauthable, omniauth_providers: [:github]
   has_many :questions
   has_many :answers
   has_many :comments
   has_many :user_opinions, dependent: :delete_all
+  has_many :authorizations, dependent: :delete_all
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth[:provider], uid: auth[:uid].to_s).first
+    return authorization.user if authorization
+
+    email = auth[:info][:email]
+
+    user = User.where(email: email).first
+    return nil unless user
+
+    user.create_authorization(auth)
+
+    user
+  end
+
+  def create_authorization(auth)
+    authorizations.create(provider: auth.provider, uid: auth.uid)
+  end
 end
