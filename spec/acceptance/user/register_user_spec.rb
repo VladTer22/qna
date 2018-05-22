c381dd156540f822aa311ee2f24f2040c73b9071
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-22 12:11:18 +0100

    Updated old tests

--- spec/acceptance/user/register_user_spec.rb
+++ spec/acceptance/user/register_user_spec.rb
@@ -12,11 +12,6 @@ feature 'Create question', '
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '123456'
   end
-  scenario 'Guest creates new account' do
-    fill_in 'Password confirmation', with: '123456'
-    click_on 'Sign up'
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
-  end
 
   scenario 'Guest enters wrong data' do
     fill_in 'Password confirmation', with: '12345'
