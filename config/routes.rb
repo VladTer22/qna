a965465538198070e28b550779c618d7d561433d
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-25 12:11:18 +0100

    Routing for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -21,6 +22,16 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resource :profiles do
+        get :me, on: :collection
+        get :all, on: :collection
+      end
+    end
+  end
+
 
   mount ActionCable.server => '/cable'
 end
