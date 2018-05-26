54c1dae704200b6f8b29f24fd31871516cc5cffc
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-26 12:11:18 +0100

    Removed default user for token

--- spec/factories/access_tokens.rb
+++ spec/factories/access_tokens.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :access_token, class: Doorkeeper::AccessToken do
     application { create(:oauth_application) }
-    resource_owner_id { create(:user).id }
+    resource_owner_id { nil }
   end
 end
