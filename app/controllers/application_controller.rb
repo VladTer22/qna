989858d82c84707d0fd01c0389676ba0e0cefda4
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-27 12:11:18 +0100

    Updated handler for cancan access denied exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,16 @@ class ApplicationController < ActionController::Base
 
   def self.render_with_signed_in_user(user, *args)
     ActionController::Renderer::RACK_KEY_TRANSLATION['warden'] ||= 'warden'
-    proxy = Warden::Proxy.new({}, Warden::Manager.new({})).tap{|i| i.set_user(user, scope: :user) }
+    proxy = Warden::Proxy.new({}, Warden::Manager.new({})).tap { |i| i.set_user(user, scope: :user) }
     renderer = self.renderer.new('warden' => proxy)
     renderer.render(*args)
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.json { render nothing: true, status: :forbidden }
+      format.html { redirect_to root_url, alert: exception.message }
+      format.js   { render nothing: true, status: :forbidden }
+    end
   end
 end
