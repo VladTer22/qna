b28b7d414b2b69ffe5be69b0e5d2dd38eab505bb
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-05-28 12:11:18 +0100

    Created base controller for api

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::BaseController < ApplicationController
+  before_action :doorkeeper_authorize!
+
+  respond_to :json
+
+  def default_serializer_options
+    {root: true}
+  end
+
+  protected
+
+  def current_resource_owner
+    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def authenticate_user!
+    if doorkeeper_token.try(:resource_owner_id)
+      Thread.current[:current_user] = User.find(doorkeeper_token.resource_owner_id)
+    end
+
+    return if current_user
+
+    render json: { errors: ['User is not authenticated!'] }, status: :unauthorized
+  end
+
+  def current_user
+    Thread.current[:current_user]
+  end
+end
