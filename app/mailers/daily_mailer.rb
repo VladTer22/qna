5b4a7cd4ca9c0d1e32951bb00c1e87608b742718
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-06-02 12:11:18 +0100

    Added variables for better email layout

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,7 +1,8 @@
 class DailyMailer < ApplicationMailer
   def digest(user)
     @questions = Question.where('created_at < ?', 1.day.ago)
-
+    @user = user
+    Rails.logger.info('sent')
     mail to: user.email
   end
 end
