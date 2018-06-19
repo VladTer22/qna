b6ac396b6c6d99abdef887c9b9c2f2b47ea26db8
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-06-19 12:11:18 +0100

    Prevented searches containing '@' as it was crashing Sphinx

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,9 @@
 class SearchesController < ApplicationController
   def search
+    if search_params[:search].include?('@')
+      redirect_to root_path, alert: "Search cannot contain '@'"
+      return
+    end
     @search = Search.new(search_params)
     @search.perform
   end
