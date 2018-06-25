bb5ac8721f2864ae250f6c1d79414ebb39ff6337
VladTer22 <volodymyrtereshchuk22@gmail.com>
2018-06-25 12:11:18 +0100

    Added redis gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,33 +24,10 @@ namespace :deploy do
   end
 
   after :publishing, :restart
-end
-
-namespace :sidekiq do
-
-  task :restart do
-    invoke 'sidekiq:stop'
-    invoke 'sidekiq:start'
-  end
-
-  before 'deploy:finished', 'sidekiq:restart'
-
-  task :stop do
-    on roles(:app) do
-      within current_path do
-        pid = p capture "ps aux | grep sidekiq | awk '{print $2}' | sed -n 1p"
-        execute("kill -9 #{pid}")
-      rescue StandardError
-        puts 'Sidekiq was not running'
-      end
-    end
-  end
-
-  task :start do
-    on roles(:app) do
-      within current_path do
-        execute :bundle, "exec sidekiq -e #{fetch(:stage)} -C config/sidekiq.yml -d"
-      end
-    end
+  task :add_default_hooks do
+    after 'deploy:starting', 'sidekiq:quiet'
+    after 'deploy:updated', 'sidekiq:stop'
+    after 'deploy:reverted', 'sidekiq:stop'
+    after 'deploy:published', 'sidekiq:start'
   end
 end
